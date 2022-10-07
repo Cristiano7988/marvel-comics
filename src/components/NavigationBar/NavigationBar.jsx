c6477cf397ed26e7cc01ccf00773b8bf2c77269e
Cristiano7988 <cristiano.morales@contractor.outroll.com>
2022-10-06 22:25:46 -0300

    ⚡️ Creating the component "NavigationBar"

---
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+const tabs = ["Characters", "Comics"];
+
+const NavigationBar = () => {
+  return (
+    <ul>
+      {tabs.map((tab, index) => (
+        <li key={index}>
+          <Link
+            children={tab}
+            to={"/" + tab.toLowerCase()}
+            className="App-link"
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default NavigationBar;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Comics from "../components/Comics";
 import Characters from "../components/Characters";
+import NavigationBar from "../components/NavigationBar";
 
 const RouteElement = () => <Router>
+    <NavigationBar />
+
     <Routes>
         <Route element={<Comics />} path="/comics" />
         <Route element={<Characters />} path="/characters" />
