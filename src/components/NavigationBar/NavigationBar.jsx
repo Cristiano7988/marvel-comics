37f9452a386a0bca4a3022e66468d698b2d446a7
Cristiano7988 <cristiano.morales@contractor.outroll.com>
2022-10-06 22:44:54 -0300

    ✨ Creating the component "Events"

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-const tabs = ["Characters", "Comics", "Creators"];
+const tabs = ["Characters", "Comics", "Creators", "Events"];
 
 const NavigationBar = () => {
   return (
